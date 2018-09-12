5202ce00d335d9c4bb6c5bed9619e6bc9688baba
ashishdhyani <me.dhyani@gmail.com>
2018-09-12 16:25:51 +0800

    first check-in deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDDuy4f9FbbT2Woismn1M9AsE-EHWzOems',
+    authDomain: 'tradeal-10b34.firebaseapp.com',
+    databaseURL: 'https://tradeal-10b34.firebaseio.com',
+    projectId: 'tradeal-10b34',
+    storageBucket: '',
+    messagingSenderId: '335737244289'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDDuy4f9FbbT2Woismn1M9AsE-EHWzOems',
+    authDomain: 'tradeal-10b34.firebaseapp.com',
+    databaseURL: 'https://tradeal-10b34.firebaseio.com',
+    projectId: 'tradeal-10b34',
+    storageBucket: '',
+    messagingSenderId: '335737244289'
+  }
 };
 
 /*
